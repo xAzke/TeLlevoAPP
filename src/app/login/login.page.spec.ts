8a8b061a2a826f88f7d6621b04a5169b10aa27a8
Diego Riquelme <diego.riquelme2016@gmail.com>
2024-11-09 12:56:33 -0300

    Iniciadas las pruebas en el formulario del login

--- src/app/login/login.page.spec.ts
+++ src/app/login/login.page.spec.ts
@@ -2,16 +2,44 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginPage } from './login.page';
 
 describe('LoginPage', () => {
-  let component: LoginPage;
-  let fixture: ComponentFixture<LoginPage>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	// Testing 1: Comprobar si se crea el formulario de inicio.
+	it('P1: Existencia de LoginPane', () => {
+		const fixture = TestBed.createComponent(LoginPage)
+		const app = fixture.componentInstance;
+
+		expect(app).toBeTruthy()
+	})
+
+	// Testing 2: Al ingresar solo el usuario el formulario debe ser invalido.
+	it('P2: Formulario Invalido', () => {
+		const fixture = TestBed.createComponent(LoginPage)
+		const app = fixture.componentInstance
+
+		const usernameControl = app.loginForm.get("username")
+		
+		// APLICAMOS UN VALOR AL CAMPO DE USUARIO
+		usernameControl?.setValue('1234567890')
+
+		// VERIFICAMOS QUE EL FORMULARIO SEA INVALIDO
+		expect(app.loginForm.invalid).toBeTrue()
+	})
+
+	// Testing 3: Usuario y contraseña son ingresados en el formulario
+	it("P3: Formulario Valido", () => {
+		const fixture = TestBed.createComponent(LoginPage)
+		const app = fixture.componentInstance
+
+		// APLICAMOS UN VALOR AL CAMPO DE USUARIO
+		const usernameControl = app.loginForm.get("username")
+		usernameControl?.setValue("Pasajero")
+
+		// APLICAMOS UN VALOR AL CAMPO DE CONTRASEÑA
+		const passwordControl = app.loginForm.get("password")
+		passwordControl?.setValue("1234")
+
+		// VERIFICAMOS QUE EL FORMULARIO SEA VALIDO
+		expect(app.loginForm.invalid).toBeFalse()
+	})
 });
